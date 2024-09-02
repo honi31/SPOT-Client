f67995f68b0b55262c0a1dc29498774c22df28cf
honi31 <winner020531@naver.com>
2024-09-03 01:13:06 +0900

    Feat - 메인 페이지 상품 카드 ui 변경

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,15 +1,20 @@
 import Category from "../components/Main/Category";
 import Header from "../components/Main/Header";
 import MainContent from "../components/Main/Main";
+import NavBar from "../components/Nav/NavBar";
 
 export default function Main() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <div className="sticky top-0 z-40">
         <Header />
         <Category />
       </div>
-      <MainContent />
+      <div className="flex-1 overflow-y-auto">
+        <MainContent />
+      </div>
+
+      <NavBar />
     </div>
   );
 }
