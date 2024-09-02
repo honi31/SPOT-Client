dd28572b0fb3e7b81eb6586fcf01b50c5c451935
honi31 <winner020531@naver.com>
2024-09-02 11:12:01 +0900

    Feat - 하단 네비게이션 바 구현
    - 홈, 채팅, 찜, 마이페이지지 nav bar 구현
    - 하단바 영향으로 글쓰기 버튼 위치 조정
    - 헤더 마이페이지 중복 아이콘 삭제

---
+++ src/components/Nav/NavBar.tsx
@@ -0,0 +1,23 @@
+import { HomeIcon } from "@heroicons/react/24/outline";
+import { ChatBubbleOvalLeftIcon } from "@heroicons/react/24/outline";
+import { HeartIcon } from "@heroicons/react/24/outline";
+import { UserIcon } from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
+export default function NavBar() {
+  return (
+    <div className="fixed bottom-0 left-0 right-0 z-50 bg-white border-t border-gray-200 flex justify-around py-4 px-1">
+      <Link to="/main">
+        <HomeIcon className="size-8 text-gray-500" />
+      </Link>
+      <Link to="/chat">
+        <ChatBubbleOvalLeftIcon className="size-8 text-gray-500" />
+      </Link>
+      <Link to="/like">
+        <HeartIcon className="size-8 text-gray-500" />
+      </Link>
+      <Link to="/mypage">
+        <UserIcon className="size-8 text-gray-500" />
+      </Link>
+    </div>
+  );
+}
