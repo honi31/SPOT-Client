8951e8a1ca6379ecea77b9c984cddab802f73c5d
honi31 <winner020531@naver.com>
2024-10-03 22:06:01 +0900

    Api - Stompjs 설치 및 웹소켓 연결 구현

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,6 +1,8 @@
+import { useEffect, useRef, useState } from "react";
 import ChatMessagesList from "../components/Chat/ChatMessageList";
 import ProductCard from "../components/Chat/ProductCard";
 import { useAuth } from "../context/AuthContext";
+import { Stomp } from "@stomp/stompjs";
 interface User {
   id: number;
   username: string;
@@ -15,7 +17,11 @@ interface Message {
   created_at: Date;
 }
 export default function Chat() {
+  const roomId = 1;
   const { isLoggedIn, logout } = useAuth();
+  const stompClient = useRef<any>(null);
+  const [message, setMessage] = useState<string>("");
+  const [messages, setMessages] = useState<Message[]>([]);
   const dummyMessages: Message[] = [
     {
       id: 1,
@@ -40,6 +46,33 @@ export default function Chat() {
       created_at: new Date(new Date().getTime() - 120000),
     },
   ];
+
+  // 웹소켓 연결 및 메시지 구독
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:80/ws");
+    stompClient.current = Stomp.over(socket);
+
+    stompClient.current.connect({}, () => {
+      // 특정 채팅방에 구독
+      stompClient.current.subscribe(
+        `/sub/chatroom/${roomId}`,
+        (message: any) => {
+          const newMessage: Message = JSON.parse(message.body);
+          setMessages((prevMessages) => [...prevMessages, newMessage]); // 새 메시지를 상태에 추가
+        }
+      );
+    });
+
+    // // 초기 메시지 로드
+    // fetchMessages();
+
+    // 컴포넌트 언마운트 시 웹소켓 연결 해제
+    return () => {
+      if (stompClient.current) {
+        stompClient.current.disconnect();
+      }
+    };
+  }, []);
 
   return (
     <>
