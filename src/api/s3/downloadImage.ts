0172fe7065bcd59279008f724150b3f2c3a03bfb
honi31 <winner020531@naver.com>
2025-01-21 22:01:12 +0900

    Feat - s3 cloudfront 이미지 가져와서 렌더링하기

--- src/api/s3/downloadImage.ts
+++ src/api/s3/downloadImage.ts
@@ -2,8 +2,8 @@ import { apiClient } from "../apiClient";
 
 export async function downloadImage(filenames: string) {
   try {
-    const response = await apiClient.get(`/aws/geturl/${filenames}`);
-    console.log("이미지 다운로드 요청 api 성공");
+    const response = await apiClient.get(`/aws/geturl?filenames=${filenames}`);
+    console.log("이미지 다운로드 요청 api 성공", response);
     return response;
   } catch (error) {
     console.log("이미지 다운로드 요청 api 에러", error);
